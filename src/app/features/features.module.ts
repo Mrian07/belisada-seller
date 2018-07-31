3c609ee0a1372970894f65fd87c6cece73572bed
macbook <macbook@Lalang-MacBook-Pro.local>
2018-07-31 15:50:47 +0700

    listing order ui

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileModule } from '@belisada-seller/features/profile/profile.module';
-import { OrderListComponent } from './order-list/order-list.component';
-import { OrderComponent } from './order/order.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [OrderListComponent, OrderComponent]
+  declarations: []
 })
 export class FeaturesModule { }
