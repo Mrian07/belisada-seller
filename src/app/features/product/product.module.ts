68d00ad1072765d8a92726398107e76701c26ec8
macbook <macbook@Lalang-MacBook-Pro.local>
2018-09-24 11:50:43 +0700

    registrasi seller

--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListingProductComponent } from './listing-product/listing-product.component';
-import { FieldErrorComponent } from './field-error/field-error.component';
 import { FieldComponent } from './add-product/field/field.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FieldComponent]
+  declarations: []
 })
 export class ProductModule { }
