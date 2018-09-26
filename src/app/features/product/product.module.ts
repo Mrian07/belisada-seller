e61daf67c9ddc7f1bd5c09ebb06a4bde59e8924a
madrick <achmad.budianto@myacico.co.id>
2018-09-26 11:06:58 +0700

    Remove field component

--- src/app/features/product/product.module.ts
+++ src/app/features/product/product.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListingProductComponent } from './listing-product/listing-product.component';
-import { FieldComponent } from './add-product/field/field.component';
 
 @NgModule({
   imports: [
