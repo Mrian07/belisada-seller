eea317fca823df1aa662060f2f99822cccb11d57
Lalang <lalang@myacico.co.id>
2018-10-17 17:00:27 +0700

    komplen seller

--- src/app/features/product-assist/product-assist.component.ts
+++ src/app/features/product-assist/product-assist.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ComplainService } from '@belisada-seller/core/services/complain/complain.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { Content } from '@belisada-seller/core/models';
+import { Content, ComplaintPDC, Accept, Reject } from '@belisada-seller/core/models';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'bss-product-assist',
@@ -11,12 +12,15 @@ import { Content } from '@belisada-seller/core/models';
 })
 export class ProductAssistComponent implements OnInit {
   list: Content[];
+  listPDC: ComplaintPDC[];
   detail: Boolean = false;
   popUpTolak: Boolean = false;
   popUpProses: Boolean = false;
   currentPage: number;
   alasanReject: string;
-  createComForm: FormGroup;
+  rejectForm: FormGroup;
+  acceptForm: FormGroup;
+  orderNumber: number;
   constructor(
     private complainSerive: ComplainService,
     private router: Router,
@@ -31,6 +35,19 @@ export class ProductAssistComponent implements OnInit {
       // this.orderList((queryParam.status) ? queryParam.status : 'ALL');
     });
     this.loadData();
+    this.loadPDC();
+    this.formReject();
+    this.formAccept();
+  }
+
+  loadPDC() {
+    const queryParams = {
+      code: 'PDC'
+    };
+
+    this.complainSerive.getPDC(queryParams).subscribe(response => {
+      this.listPDC = response;
+    });
   }
 
   loadData() {
@@ -42,13 +59,87 @@ export class ProductAssistComponent implements OnInit {
 
     this.complainSerive.getComplain(queryParams).subscribe(response => {
       this.list = response.content;
-      console.log('hasi sih', response.content);
     });
   }
 
   formReject() {
-      this.createComForm = this.fb.group({
-        alasanReject: ['', [Validators.required]]
+      this.rejectForm = this.fb.group({
+        orderNumber: ['', [Validators.required]],
+        rejectReason: ['', [Validators.required]],
+      });
+  }
+
+  formAccept() {
+    this.acceptForm = this.fb.group({
+      orderNumber: ['', [Validators.required]],
+      complainAcceptCode: ['', [Validators.required]],
+    });
+  }
+
+  submitReject() {
+    if (this.rejectForm.controls['rejectReason'].value === '') {
+      swal(
+        'Peringatan',
+        'Silakan masukan alasan penolakan',
+        'warning'
+      );
+      return;
+    }
+    this.popUpTolak = false;
+      const rejectData: Reject = new Reject();
+      rejectData.orderNumber = this.rejectForm.controls['orderNumber'].value;
+      rejectData.rejectReason = this.rejectForm.controls['rejectReason'].value;
+
+      this.complainSerive.rejectComplain(rejectData).subscribe(response => {
+        console.log('respon', response);
+        this.popUpTolak = false;
+
+        if (response.status === 1) {
+          swal(
+            'Peringatan',
+            'Tolak komplain berhasil dilakukan',
+            'success'
+          );
+        } else {
+          swal(
+            'Peringatan',
+            'Tolak komplain gagal dilakukan',
+            'warning'
+          );
+        }
+      });
+  }
+
+  submitAccept() {
+
+    if (this.acceptForm.controls['complainAcceptCode'].value === '') {
+      swal(
+        'Peringatan',
+        'Silakan pilih opsi pengembalian dana',
+        'warning'
+      );
+      return;
+    }
+    this.popUpProses = false;
+    const acceptData: Accept = new Accept();
+      acceptData.orderNumber = this.acceptForm.controls['orderNumber'].value;
+      acceptData.complainAcceptCode = this.acceptForm.controls['complainAcceptCode'].value;
+      this.complainSerive.acceptComplain(acceptData).subscribe(response => {
+        console.log('respon', response);
+        this.popUpTolak = false;
+        if (response.status === 1) {
+          swal(
+            'Peringatan',
+            'Proses komplain berhasil dilakukan',
+            'success'
+          );
+        } else {
+          swal(
+            'Peringatan',
+            'Proses komplain gagal dilakukan',
+            'warning'
+          );
+        }
       });
   }
 
@@ -56,12 +147,20 @@ export class ProductAssistComponent implements OnInit {
     this.detail = true;
   }
 
-  btnTolak() {
+  btnTolak(orderNumber) {
     this.popUpTolak = true;
+    this.rejectForm.patchValue(
+      {
+        orderNumber: orderNumber,
+      });
   }
 
-  btnProses() {
+  btnProses(orderNumber) {
     this.popUpProses = true;
+    this.acceptForm.patchValue(
+      {
+        orderNumber: orderNumber,
+      });
   }
 
   cancelTolak() {
