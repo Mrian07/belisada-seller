e2e366d40e3561c063ed6dc7e28f6fc7be38ed90
Lalang <lalang@myacico.co.id>
2018-12-27 14:48:33 +0700

    validasi varian add product

--- src/app/features/product/add-product-v2/add-product-v2.component.ts
+++ src/app/features/product/add-product-v2/add-product-v2.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductsSandbox } from '../products.sandbox';
 import { Subscription } from 'rxjs';
 import { AddProductRequest, Reference, VariantAttr, Variant, ProductCreate, ProductDetailData } from '@belisada-seller/core/models';
-import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, AbstractControl, FormControl } from '@angular/forms';
 
 import { environment } from '@env/environment';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProductService } from '@belisada-seller/core/services';
 import swal from 'sweetalert2';
+import { LoadingService } from '@belisada-seller/core/services/globals/loading.service';
 
 function compareTwoPrices(c: AbstractControl) {
   if (c.get('specialPrice').value === '' || c.get('pricelist').value === '') {
@@ -58,6 +59,7 @@ export class AddProductV2Component implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private productService: ProductService,
     private router: Router,
+    private loadingService: LoadingService,
   ) {
     this.productStoreUrl = environment.thumborUrl + 'unsafe/50x50/center/filters:fill(fff)/';
     this.productImage = environment.thumborUrl + 'unsafe/fit-in/400x400/center/filters:fill(fff)/';
@@ -75,6 +77,7 @@ export class AddProductV2Component implements OnInit, OnDestroy {
       masterId:  [''],
       varians: this.fb.array([]),
     });
+
     this.productsSandbox.courier();
 
     this.activatedRoute.params.subscribe((params: Params) => {
@@ -135,6 +138,20 @@ export class AddProductV2Component implements OnInit, OnDestroy {
         });
       }
     });
+  }
+
+  toggleControl(variant: FormArray) {
+    const isUsed = variant.controls['isUsed'].value;
+    if (isUsed === false) {
+      variant.controls['pricelist'].disable();
+      variant.controls['specialPrice'].disable();
+      variant.controls['qty'].disable();
+    } else {
+      variant.controls['pricelist'].enable();
+      variant.controls['specialPrice'].enable();
+      variant.controls['qty'].enable();
+    }
+    console.log(variant.controls['isUsed'].value);
   }
 
   private _initVariants(): FormGroup {
@@ -219,30 +236,36 @@ export class AddProductV2Component implements OnInit, OnDestroy {
   }
 
   public postProductV2() {
+
+    // this.loadingService.show();
     this.submitted = true;
     this.calculateDiscount();
 
-
-
     const control = <FormArray>this.addProductForm.get('varians');
-    const b = control.value.filter(item => item.isUsed !== false);
-    // console.log(b);
+    const variantsOnlyChecked = control.value.filter(item => item.isUsed !== false);
+    console.log('variantsOnlyChecked: ', variantsOnlyChecked);
 
     // console.log('control: ', control);
 
+    const variantsControls = <FormArray>this.getVariants(this.addProductForm);
+    console.log('variantsControls: ', variantsControls);
 
-    const a = this.addProductForm.value;
-    a.varians = b;
+    // return;
+    const productFormModified = this.addProductForm.value;
+    productFormModified.varians = variantsOnlyChecked;
+
+    console.log('productFormModified: ', productFormModified);
 
 
-    const c = b;
-    const xx = b.forEach(element => {
+    const c = variantsOnlyChecked;
+    const xx = variantsOnlyChecked.forEach(element => {
       if (element.pricelist < 100) {
       swal(
         'Warning',
         'harga tidak boleh dibawah 100',
         'warning'
       );
+      this.loadingService.hide();
       return;
       }
     });
@@ -253,40 +276,47 @@ export class AddProductV2Component implements OnInit, OnDestroy {
         'Metode pengiriman harus diisi',
         'warning'
       );
+      this.loadingService.hide();
       return;
     }
 
+    console.log('hasilnya', this.addProductForm.valid);
+    console.log('isi hasilnya', this.addProductForm.value);
+
     if (this.addProductForm.valid) {
-      console.log('ini kalo benar');
       if ( this.router.url === '/edit-products/' + this.masterId) {
         console.log('this.addProductForm.value-asd---: ', this.addProductForm.value);
-        this.productService.editProductPost(a).subscribe(response => {
+        this.productService.editProductPost(productFormModified).subscribe(response => {
           // console.log(response);
-          swal(
-          'belisada.co.id',
-          response.message,
-          (response.status === 0) ? 'error' : 'success'
-        );
-        if (response.status === 1) {
-          this.router.navigate(['/listing-product']);
-        }
-      });
-    } else {
-      console.log('this.addProductForm.value----: ', this.addProductForm.value);
-      this.productService.addProductV2(a).subscribe(response => {
-        // console.log(response);
-        swal(
-        'belisada.co.id',
-        response.message,
-        (response.status === 0) ? 'error' : 'success'
-      );
-      if (response.status === 1) {
-        this.router.navigate(['/listing-product']);
+            swal(
+            'belisada.co.id',
+            response.message,
+            (response.status === 0) ? 'error' : 'success'
+          );
+          if (response.status === 1) {
+            this.router.navigate(['/listing-product']);
+            this.loadingService.hide();
+          }
+        });
+      } else {
+        console.log('this.addProductForm.value----: ', this.addProductForm.value);
+        this.productService.addProductV2(productFormModified).subscribe(response => {
+          // console.log(response);
+            swal(
+            'belisada.co.id',
+            response.message,
+            (response.status === 0) ? 'error' : 'success'
+          );
+          if (response.status === 1) {
+            this.router.navigate(['/listing-product']);
+            this.loadingService.hide();
+          }
+        });
+
       }
-    });
-    }
     } else {
-      console.log('ini kalo salah');
+      console.log('salah nih');
+      this.loadingService.hide();
     }
   }
   calculateDiscount() {
